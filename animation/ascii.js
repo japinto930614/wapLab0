fa0fa5a2bc9209bfcedeaf79cb99b9149c769e83
jorge <jorge_930614@hotmail.com>
2019-06-04 16:02:07 -0500

    Animation

--- animation/ascii.js
+++ animation/ascii.js
@@ -1,23 +1,37 @@
-
-
+var index = 0;
+function putText(animation) {
+    let textbox = document.getElementById("textbox");
+    var animationArray = ANIMATIONS[animation].split("=====\n");
+    textbox.value = animationArray[index];
+    ++index;
+    if (index == animationArray.length) {
+        index = 0;
+    }
+}
 
 window.onload = function () {
-    
-    document.getElementById("biggerbutton").onclick = function () {
-        if(stop){
-            stop = false;
-            start = setInterval(function(){ makeBigger(); }, 500);
-            
-        } else {
-            stop = true;
-            clearInterval(start);
+    let play;
+    let isPlaying = false;
+    let sizeoption = document.getElementById("sizeoption");
+    let speed
+    document.getElementById("startbutton").onclick = function () {
+        if (!isPlaying) {
+            animation = document.getElementById("animationoption").value;
+            if (animation != "") {
+                play = setInterval(function () { putText(animation); }, 250);
+                isPlaying = true;
+            }
         }
-        
-        
     };
 
-    document.getElementById("blingcheck").onchange = function() {
-        bling();
-    };
-  
+    document.getElementById("stopbutton").onclick = function () {
+        clearInterval(play);
+        isPlaying = false;
+    }
+
+    sizeoption.onchange = function () {
+        let textbox = document.getElementById("textbox").style.fontSize = sizeoption.value;
+    }
+
+
 }
