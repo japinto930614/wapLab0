61d68270ac5c7b2073c70e691bb87a1d0c84da65
jorge <jorge_930614@hotmail.com>
2019-06-05 14:13:40 -0500

    Finished animation

--- animation/ascii.js
+++ animation/ascii.js
@@ -15,16 +15,19 @@ window.onload = function () {
     let sizeoption = document.getElementById("sizeoption");
     let startbutton = document.getElementById("startbutton");
     let stopbutton = document.getElementById("stopbutton");
+    let animationoption = document.getElementById("animationoption");
+    let turbocheck = document.getElementById("turbocheck");
     let speed = 250;
    
 
     startbutton.onclick = function () {
         if (!isPlaying) {
-            animation = document.getElementById("animationoption").value;
+            animation = animationoption.value;
             if (animation != "") {
                 play = setInterval(function () { putText(animation); }, speed);
                 startbutton.disabled = true;
                 stopbutton.disabled = false;
+                animationoption.disabled = true;
                 isPlaying = true;
             }
         }
@@ -33,9 +36,21 @@ window.onload = function () {
         clearInterval(play);
         startbutton.disabled = false;
         stopbutton.disabled = true;
+        animationoption.disabled = false;
         isPlaying = false;
     }
     sizeoption.onchange = function () {
         let textbox = document.getElementById("textbox").style.fontSize = sizeoption.value;
+    }
+    turbocheck.onchange = function () {
+        if(turbocheck.checked){
+            speed = 50;
+            clearInterval(play);
+            play = setInterval(function () { putText(animation); }, speed);
+        } else {
+            speed = 250;
+            clearInterval(play);
+            play = setInterval(function () { putText(animation); }, speed);
+        }
     }
 }
